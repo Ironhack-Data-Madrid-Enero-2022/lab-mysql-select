04bdd1d45c2c00861e89036a6c6d947fadce7c61
IsabelGarcia11 <isabel.garciaquereda@gmail.com>
2022-02-01 17:58:35 +0100

    version final

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -22,8 +22,8 @@ group by title
 
 --CHALLENGE 3
 select a.au_id as AUTHOR_ID,a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME,sum(sa.qty) as TOTAL
-from titleauthor as t
-left join authors as a
+from authors as a
+left join titleauthor as t
 on a.au_id=t.au_id
 left join titles as ti
 on ti.title_id=t.title_id
@@ -31,9 +31,20 @@ left join publishers as pu
 on pu.pub_id=ti.pub_id
 left join sales as sa
 on sa.title_id=ti.title_id
-group by title
+group by a.au_id
 order by TOTAL DESC 
 LIMIT 3
 
 --CHALLENGE 4
-
+select a.au_id as AUTHOR_ID,a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME,ifnull(sum(sa.qty),0) as TOTAL
+from authors as a
+left join titleauthor as t
+on a.au_id=t.au_id
+left join titles as ti
+on ti.title_id=t.title_id
+left join publishers as pu
+on pu.pub_id=ti.pub_id
+left join sales as sa
+on sa.title_id=ti.title_id
+group by a.au_id
+order by TOTAL DESC 
